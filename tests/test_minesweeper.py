96d6762f85bba2bf636804c77a09b5d08f04fa8b
Thibaut Corallo <thibaut.corallo@artefact.com>
2026-03-05 15:52:57 +0000

    Test du pre-commit

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,9 +1,11 @@
 import pytest
+
 from src import minesweeper
 
 
 def test_module_exists():
     assert minesweeper
+
 
 def test_place_mines():
     game = minesweeper.Minesweeper(3, 3, 2)
@@ -14,8 +16,10 @@ if __name__ == "__main__":
     game = minesweeper.Minesweeper(3, 3, 2)
     print(game)
 
+
 def test_reveal():
     import random
+
     random.seed(0)
     game = minesweeper.Minesweeper(3, 3, 2)
     game.place_mines()
